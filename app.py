0dd7145ff19877b52a659c9f2535060a06b88d97
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-09-02 00:24:08 -0400

    Heathens, rejoice.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 import datetime
 import json
 
-from flask import Flask
+from flask import Flask, render_template
 from pymongo import MongoClient
 
 app = Flask("panopticon")
@@ -13,8 +13,13 @@ db = client.apps
 panopticon = db.panopticon
 
 
-@app.route('/panopticon/raw/')
-def raw_find():
+@app.route('/panopticon/')
+def index():
+    return render_template('map.html')
+
+
+@app.route('/panopticon/<format>/')
+def raw_find(format="raw"):
     from flask import request
     q = request.args.get('q', None)
 
@@ -23,15 +28,41 @@ def raw_find():
         query = panopticon.find(json.loads(q))
 
     response = {}
-    response['items'] = []
-    start = datetime.datetime.now()
-    for item in query:
-        del(item['_id'])
-        response['items'].append(item)
-    response['meta'] = {}
-    response['meta']['count'] = len(response['items'])
-    elapsed = datetime.datetime.now() - start
-    response['meta']['time_elapsed'] = float('%s.%s' % (elapsed.seconds, elapsed.microseconds))
+
+    if format == "raw":
+        response['items'] = []
+        start = datetime.datetime.now()
+        for item in query:
+            del(item['_id'])
+            response['items'].append(item)
+        response['meta'] = {}
+        response['meta']['count'] = len(response['items'])
+        elapsed = datetime.datetime.now() - start
+        response['meta']['time_elapsed'] = float('%s.%s' % (elapsed.seconds, elapsed.microseconds))
+
+    if format == "geojson":
+        response['type'] = "FeatureCollection"
+        response['features'] = []
+        if query.count() > 0:
+            for day in query:
+                segments = day.get('segments', None)
+                if segments:
+                    for segment in segments:
+                        activities = segment.get('activities', None)
+                        if activities:
+                            for activity in activities:
+                                track_points = activity.get('trackPoints', None)
+                                if track_points:
+                                    line_dict = {}
+                                    line_dict['properties'] = {}
+                                    line_dict['geometry'] = {}
+                                    line_dict['type'] = "Feature"
+                                    line_dict['geometry']['type'] = "LineString"
+                                    line_dict['geometry']['coordinates'] = []
+                                    for point in track_points:
+                                        line_dict['geometry']['coordinates'].append([point['lon'], point['lat']])
+                                        response['features'].append(line_dict)
+
     return json.dumps(response)
 
 if __name__ == '__main__':
